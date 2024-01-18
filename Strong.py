5cab2888eaaff1c9327e692f4eca703dae95a69b
deepak90221 <116907628+deepak90221@users.noreply.github.com>
2024-01-18 14:36:46 +0530

    Update Strong.py

--- Strong.py
+++ Strong.py
@@ -1,3 +1,10 @@
-n=1
-m=2
-print(n+m)
+# This program adds two numbers
+
+num1 = 1.5
+num2 = 6.3
+
+# Add two numbers
+sum = num1 + num2
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
